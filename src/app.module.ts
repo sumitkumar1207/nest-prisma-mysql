3bef2adeb77b800ba5d001c8119f13f9682c72ed
Sumit Yadav <mackbook@Sumits-MacBook-Pro.local>
2022-04-25 14:29:00 +0530

    Required moules added in the app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoursesController } from './courses/courses.controller';
 import { CoursesModule } from './courses/courses.module';
+import { CoursesService } from './courses/courses.service';
+import { DBService } from './db/db.service';
 
 @Module({
-  imports: [CoursesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  // imports: [CoursesModule],
+  imports: [],
+  controllers: [AppController, CoursesController],
+  providers: [AppService, CoursesService, DBService],
 })
-export class AppModule {}
+export class AppModule { }
