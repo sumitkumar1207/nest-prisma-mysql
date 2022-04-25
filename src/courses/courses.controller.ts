3bc0be25eadcb2c86909f66c459a01e6cc104d5f
Sumit Yadav <mackbook@Sumits-MacBook-Pro.local>
2022-04-25 14:28:35 +0530

    Course controller added.

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+  constructor(private courseService: CoursesService) { }
+  // GET courses
+  @Get()
+  async getCourses() {
+    return await this.courseService.getCourses()
+  }
+  // Get course by id
+  @Get(':id')
+  async getCourse(@Param('id') id) {
+    return await this.courseService.getCourse(id)
+  }
+  // Add course
+  @Post()
+  async addCourse(@Body() createCourseDto: {}) {
+    return await this.courseService.addCourse(createCourseDto)
+  }
+  // Remove the course
+  @Delete()
+  async deleteCourse(@Query() query) {
+    const { id } = query
+    return await this.courseService.deleteCourse(id)
+  }
+}
